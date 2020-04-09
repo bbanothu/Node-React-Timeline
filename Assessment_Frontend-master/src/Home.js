3659ca29404302bffe9ec611a8a00feb0659d7d3
Bharath banothu <bbanothu1997@gmail.com>
2020-04-08 21:07:44 -0700

    made temporary home page - it reads in posts from the db.

---
+++ Assessment_Frontend-master/src/Home.js
@@ -0,0 +1,97 @@
+import React, { useState, Component }from 'react';
+import { AppBar, TextField, Button, Toolbar, Typography, Card, CardActions, CardContent } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Async from "react-async"
+import './App.css';
+
+// const useStyles = makeStyles({
+//   root: {
+//     minWidth: 275,
+//     marginTop: 25
+//   },
+// });
+
+
+class Home extends Component {
+    // Single fetch
+    loadJson = () =>
+        fetch("http://localhost:3000/api/getAllPosts")
+            .then(res => (res.ok ? res : Promise.reject(res)))
+            .then(res => res.json())
+
+    // Render Function      
+    render() {
+
+        const mystyle = {
+            root: {
+              minWidth: 275,
+              marginTop: 25
+            },
+          }
+        const post = (posts) => {
+            if (posts == null) {
+                return (
+                    <div></div>
+                )
+            } else {
+                return (
+                    console.log(posts),
+                    <Card className={mystyle}>
+                        {posts.map((post, index) => (
+                            <div key={index} >
+                                <CardContent className={mystyle.comment}>
+                                    <Typography >
+                                        {post.message}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Like</Button>
+                                    <Button size="small">Delete</Button>
+                                </CardActions>
+                            </div>
+                            ))}
+                    </Card>
+                )
+            }
+        }
+
+        return (
+            <Async promiseFn={this.loadJson}>
+                {({ data, error, isLoading }) => {
+                    // if (isLoading) return <div style={{ marginTop: "3em" }}>
+                    //     <img alt="loading" src={Loading} style={{
+                    //         display: "block",
+                    //         marginLeft: "auto",
+                    //         marginRight: "auto"
+                    //     }} ></img></div>
+                    // if (error) return `Something went wrong: ${error.message}`
+                    if (data)
+                        return (
+                            <div className="App">
+                            <AppBar position="static">
+                              <Toolbar>
+                                <Typography variant="h6">
+                                  FINBACK670 takeee
+                                </Typography>
+                              </Toolbar>
+                            </AppBar>
+                            <header className="App-header">
+                              <div className="Newsfeed">
+                                <TextField
+                                  id="standard-multiline-flexible"
+                                  label="New Post"
+                                  multiline
+                                />
+                                <Button variant="contained">Submit Post</Button>
+                                {post(data.data)}
+                              </div>
+                            </header>
+                          </div>
+                        )
+                    return null
+                }}
+            </Async>
+        );
+    }
+}
+export default Home;
